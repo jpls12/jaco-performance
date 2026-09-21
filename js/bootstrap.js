d063fb56c97becd9f4163061c5d12b8fab975227
jpls12 <jacopols94@gmail.com>
2026-09-21 15:31:27 +0200

    8.3.4: voer racefallback-cleanup uit bij startup

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -176,6 +176,7 @@ async function initializeJacoPerformance(){
   repairStoredWorkoutMismatches();
   installHmAmsterdamBlock2026();
   installHmAmsterdamRaceweek2026();
+  cleanupImportedRaceFallbacks();
   const greeting=document.getElementById("greeting");
   if(greeting){
     greeting.textContent=
